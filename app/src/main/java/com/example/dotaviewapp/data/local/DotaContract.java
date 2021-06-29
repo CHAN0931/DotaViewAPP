e1e91fb2960189494d27befeb27fb10cd63cb73a
CHAN0931 <ashrky143@gmail.com>
2021-06-30 00:29:34 +0900

    changed wrapper

--- app/src/main/java/com/example/dotaviewapp/data/local/DotaContract.java
+++ app/src/main/java/com/example/dotaviewapp/data/local/DotaContract.java
@@ -5,7 +5,7 @@ import android.provider.BaseColumns;
 
 public class DotaContract {
 
-    public static final String CONTENT_AUTHORITY = "com.nikola.jakshic.truesight";
+    public static final String CONTENT_AUTHORITY = "com.example.dotaviewapp";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
     public static final String PATH_ACC = "account";
 
